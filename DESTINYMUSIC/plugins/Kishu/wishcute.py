2efc564fcdc2e842df27bd69591fe8363785a911
Sandeep sharma <rs3268840@gmail.com>
2025-05-25 18:29:25 +0530

    Update wishcute.py

--- DESTINYMUSIC/plugins/Kishu/wishcute.py
+++ DESTINYMUSIC/plugins/Kishu/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from DESTINYMUSIC import app
 
-SUPPORT_CHAT = "CERTIFIEDCODERS"
+SUPPORT_CHAT = "https://t.me/+AzKGhJreNmhiZTll"
 SUPPORT_BTN = InlineKeyboardMarkup(
     [[InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}")]]
 )
